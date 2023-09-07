5e2ef401d90690a16990be8cd0434581942c5a4f
Ashok05062005 <144311325+Ashok05062005@users.noreply.github.com>
2023-09-07 16:43:03 +0530

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,10 @@
+#Implement a recursive function to calculate the factorial
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Enter a number: "))
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
